ac4ca38c92b37c714b3da0ac5f62c77f772f1fcd
Anil Gade <gadheanil57@gmail.com>
2025-08-31 09:35:11 +0530

    user-dashboard

--- src/video-Tutorial/user-dashboard.jsx
+++ src/video-Tutorial/user-dashboard.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { createContext, useState } from 'react'
+import { Vediodata } from './vedio-data';
 
-export  function Userdashboard() {
+export let userContext = createContext(null);
+
+export function Userdashboard() {
+
+  const[temp,setTemp]=useState('');
+  const[searchInp , setSearchInp]=useState('');
+
+function handleChange(e){
+ setTemp(e.target.value);
+}
+
+function handleSearchClick(){
+    setSearchInp(temp);
+}
+
+function handelChildData(e){
+
+}
+
   return (
-    <div className='text-center text-white fs-1'>Userdashboard</div>
+    <div>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary ">
+        <div className="container-fluid d-flex justify-content-evenly">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <div className="d-flex input-group w-50" role="search">
+              <input className="form-control" type="search" onChange={handleChange} placeholder="Search" aria-label="Search" />
+              <button onClick={handleSearchClick} className="btn btn-warning bi bi-search" type="submit"></button>
+            </div>
+          </div>
+          <div className='navbar-nav'>
+            <button className=' nav-item btn btn-warning bi bi-cart4'></button>
+          </div>
+        </div>
+      </nav>
+
+      <section>
+          <userContext.Provider value={searchInp}>
+             <Vediodata onChildClick={handelChildData}/>
+          </userContext.Provider>
+      </section>
+
+    </div>
   )
 }
---
+++ src/video-Tutorial/vedio-data.jsx
@@ -0,0 +1,148 @@
+
+import { useContext, useEffect, useState } from 'react'
+import { userContext } from './user-dashboard'
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import axios from 'axios'
+
+
+
+const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+})(({ theme }) => ({
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+    }),
+    variants: [
+        {
+            props: ({ expand }) => !expand,
+            style: {
+                transform: 'rotate(0deg)',
+            },
+        },
+        {
+            props: ({ expand }) => !!expand,
+            style: {
+                transform: 'rotate(180deg)',
+            },
+        },
+    ],
+}));
+
+
+export function Vediodata({ onChildClick }) {
+
+    const [expanded, setExpanded] = useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
+    const [vedios, setVedios] = useState([{
+        title: "",
+        description: "",
+        url: null,
+        likes: 0,
+        dislikes: 0,
+        views: 0,
+        comments: '',
+        category_id: '',
+        id: null
+    }],)
+
+    let context = useContext(userContext);
+
+     
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/videos').then(response => setVedios(response.data))
+            .catch((err) => {
+                console.log("failed to  load vedios:", err);
+            })
+    }, [])
+
+ let stringData = context.toLowerCase();
+
+    return (
+        <div className="d-flex flex-wrap">
+
+            {
+                vedios.filter(vedio => {
+                  return stringData == "" || vedio.title.toLowerCase().includes(stringData);
+                }).map(data => (
+                    <Card key={data.id} className='m-2'  sx={{ maxWidth: 350 }}>
+                        <CardHeader
+                            title={data.title}
+                        />
+                        <CardMedia
+                            component="iframe"
+                            src={data.url}
+                            height="194"
+                            title="YouTube video"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        />
+                        <CardContent>
+                            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                                  Description : {data.description}
+                            </Typography>
+                        </CardContent>
+                        <CardActions disableSpacing>
+                            <IconButton aria-label="add to favorites">
+                                <FavoriteIcon />
+                            </IconButton>
+                            <IconButton aria-label="share">
+                                <ShareIcon />
+                            </IconButton>
+                            <ExpandMore
+                                expand={expanded}
+                                onClick={handleExpandClick}
+                                aria-expanded={expanded}
+                                aria-label="show more"
+                            >
+                                <ExpandMoreIcon />
+                            </ExpandMore>
+                        </CardActions>
+                        <Collapse in={expanded} timeout="auto" unmountOnExit>
+                            <CardContent>
+                                <Typography sx={{ marginBottom: 2 }}>
+                                    <span className='bi bi-hand-thumbs-up'>{data.likes}</span>
+
+                                </Typography>
+                                <Typography sx={{ marginBottom: 2 }}>
+                                    <span className='bi bi-hand-thumbs-down'>{data.dislikes}</span>
+                                </Typography>
+                                <Typography className="bi bi-eye" sx={{ marginBottom: 2 }}>
+                                    {data.views}
+                                </Typography>
+                                <Typography sx={{ marginBottom: 2 }}>
+                                    {data.comments}
+                                </Typography>
+
+                            </CardContent>
+                        </Collapse>
+                    </Card>
+
+                ))
+
+            }
+
+        </div>
+    )
+}
